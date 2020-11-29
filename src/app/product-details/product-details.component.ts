1012104aa0811be0f917efc0754249b17f22c35b
Min Ku Nam <mxn1020@M1137.local>
2020-11-28 23:07:46 -0600

    first commit

---
+++ src/app/product-details/product-details.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { products } from "../products";
+import { CartService } from "../cart.service";
+
+@Component({
+  selector: "app-product-details",
+  templateUrl: "./product-details.component.html",
+  styleUrls: ["./product-details.component.css"]
+})
+export class ProductDetailsComponent implements OnInit {
+  //property
+  product;
+
+  // best pratice, dont declare in constructor, best in the ngOnInit
+  constructor(
+    private route: ActivatedRoute,
+    private cartService: CartService
+  ) {}
+
+  ngOnInit() {
+    // ActivatedRoute is service, so you need to subscribe to that service
+    this.route.paramMap.subscribe(params => {
+      this.product = products[+params.get("productId")];
+    });
+  }
+
+  addToCart(product) {
+    this.cartService.addToCart(product);
+    window.alert("your product has been added to the cart!");
+  }
+}
